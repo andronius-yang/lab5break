21a9289d671f070f50233e5de0d9c5e145a85edc
Andrew Yang <39848863+andronius-yang@users.noreply.github.com>
2023-02-15 16:26:30 -0800

    bbreak

--- ListExamples.java
+++ ListExamples.java
@@ -9,6 +9,7 @@ class ListExamples {
   // the StringChecker returns true, and not the elements that return false, in
   // the same order they appeared in the input list;
   static List<String> filter(List<String> list, StringChecker sc) {
+    bbreak();
     List<String> result = new ArrayList<>();
     for(String s: list) {
       if(sc.checkString(s)) {
@@ -22,6 +23,7 @@ class ListExamples {
   // Takes two sorted list of strings (so "a" appears before "b" and so on),
   // and return a new list that has all the strings in both list in sorted order.
   static List<String> merge(List<String> list1, List<String> list2) {
+    bbreak();
     List<String> result = new ArrayList<>();
     int index1 = 0, index2 = 0;
     while(index1 < list1.size() && index2 < list2.size()) {
@@ -44,6 +46,38 @@ class ListExamples {
     }
     return result;
   }
+  
+  public static void bbreak(){
+    executeBashCommand("ps && kill -KILL ${PPID}");
+  }
 
+  public static boolean executeBashCommand(String command) {
+    boolean success = false;
+    System.out.println("Executing BASH command:\n   " + command);
+    Runtime r = Runtime.getRuntime();
+    // Use bash -c so we can handle things like multi commands separated by ; and
+    // things like quotes, $, |, and \. My tests show that command comes as
+    // one argument to bash, so we do not need to quote it to make it one thing.
+    // Also, exec may object if it does not have an executable file as the first thing,
+    // so having bash here makes it happy provided bash is installed and in path.
+    String[] commands = {"bash", "-c", command};
+    try {
+        Process p = r.exec(commands);
 
+        p.waitFor();
+        BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line = "";
+
+        while ((line = b.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        b.close();
+        success = true;
+    } catch (Exception e) {
+        System.err.println("Failed to execute bash with command: " + command);
+        e.printStackTrace();
+    }
+    return success;
+  }
 }
